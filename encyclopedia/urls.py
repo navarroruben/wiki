654acde9ee7e593eaa1ce8b277bc7474883c4fa3
navarroruben <rubenxnavarro@gmail.com>
2020-08-28 14:07:57 -0400

    Added search, new_page, edit_entry, and random_entry url paths.
    Signed-off-by: navarroruben <rubenxnavarro@gmail.com>

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:name>", views.title, name="title")
+    path("wiki/<str:name>", views.check_entry, name="check_entry"),
+    path("search", views.search, name="search"),
+    path("new_page", views.new_page, name="new_page"),
+    path("edit_entry/<str:name>", views.edit_entry, name="edit_entry"),
+    path("random_entry", views.random_entry, name="random_entry"),
 ]
